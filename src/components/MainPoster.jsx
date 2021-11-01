04caeb9f5a3ab79c70348de96ba0c89d5d7c8438
Santhosh <santhoshm12052003@gmail.com>
2021-11-01 13:17:42 +0530

    main poster size adjusted. about club font-size increased(desktop)

--- src/components/MainPoster.jsx
+++ src/components/MainPoster.jsx
@@ -7,7 +7,7 @@ function MainWorks() {
         <div id='main'>
             <h1 className="prakruti">PRAKRUTI</h1>
             <b>The Nature Club of NIT Tiruchippalli</b>
-            <p>All creatures deserve a life free from fear and pain. It's true that the greatness of a nation can be judged by the way its animals are treated. So show some love towards the flora and fauna around you to make ours a lovely nation <FaHeart color='red'></FaHeart></p>
+            <div>All creatures deserve a life free from fear and pain. It's true that the greatness of a nation can be judged by the way its animals are treated. So show some love towards the flora and fauna around you to make ours a lovely nation <FaHeart color='red'></FaHeart></div>
         </div>
         </div>
     );
